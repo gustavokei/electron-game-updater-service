c7d43d3175d26a3d9e329eecd1077823cb489ef0
Gustavo Kei Amemiya <gustavokeia@gmail.com>
2024-07-16 21:21:10 -0300

    first release

---
+++ cron.js
@@ -0,0 +1,31 @@
+require('dotenv').config(); // Load environment variables from .env file
+
+const cron = require('cron');
+const https = require('https');
+
+const backendUrl = process.env.BACKEND_URL;
+const keepAlive = new cron.CronJob('*/1 * * * *', function () {
+    // This function will be executed every 14 minutes.
+    console.log(`Fetching server`);
+
+    // Perform an HTTPS GET request to hit any backend API.
+    https
+        .get(backendUrl, (res) => {
+            if (res.statusCode === 200) {
+                console.log('Server fetched');
+                console.log(res)
+            } else {
+                console.error(
+                    `Failed to fetch server with status code: ${res.statusCode}`
+                );
+            }
+        })
+        .on('error', (err) => {
+            console.error('Error during Fetch:', err.message);
+        });
+});
+
+// Export the cron job.
+module.exports = {
+    start: () => keepAlive.start()
+};
---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config(); // Load environment variables from .env file
+
+const keepAlive = require('./cron.js'); // Adjust path if necessary
+keepAlive.start();
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Define a route for GET requests to "/"
+app.get('/', (req, res) => {
+  res.send({
+    startCmd: process.env.START_CMD,
+    launcherVer: process.env.LAUNCHER_VER,
+    clientVer: process.env.CLIENT_VER,
+    patchVer: process.env.PATCH_VER,
+    launcherUrl: process.env.LAUNCHER_URL,
+    clientUrl: process.env.CLIENT_URL,
+    patchUrl: process.env.PATCH_URL,
+  });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
