452b7248cd7e27ace81910ef8e98cd66c1156250
Gustavo Kei Amemiya <gustavokeia@gmail.com>
2024-07-17 20:48:28 -0300

    fix: fixing cors

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
+
+
 // Define a route for GET requests to "/"
 app.get('/', (req, res) => {
   res.send({
