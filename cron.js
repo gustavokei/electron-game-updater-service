6555bf6930802a3d1867b360f265286a993deff2
Gustavo Kei Amemiya <gustavokeia@gmail.com>
2024-07-16 21:43:42 -0300

    chore: testing data output

--- cron.js
+++ cron.js
@@ -1,24 +1,35 @@
 require('dotenv').config(); // Load environment variables from .env file
 
 const cron = require('cron');
-const https = require('https');
+const http = require('http');
 
 const backendUrl = process.env.BACKEND_URL;
 const keepAlive = new cron.CronJob('*/1 * * * *', function () {
-    // This function will be executed every 14 minutes.
+    // This function will be executed every 1 minute.
     console.log(`Fetching server`);
 
-    // Perform an HTTPS GET request to hit any backend API.
-    https
+    // Perform an HTTP GET request to hit any backend API.
+    http
         .get(backendUrl, (res) => {
-            if (res.statusCode === 200) {
-                console.log('Server fetched');
-                console.log(res)
-            } else {
-                console.error(
-                    `Failed to fetch server with status code: ${res.statusCode}`
-                );
-            }
+            let data = '';
+
+            // Collect data chunks.
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            // When the response is complete, log the result.
+            res.on('end', () => {
+                if (res.statusCode === 200) {
+                    console.log('Server fetched');
+                    console.log('Response:', data);
+                } else {
+                    console.error(
+                        `Failed to fetch server with status code: ${res.statusCode}`
+                    );
+                    console.error('Response:', data);
+                }
+            });
         })
         .on('error', (err) => {
             console.error('Error during Fetch:', err.message);
