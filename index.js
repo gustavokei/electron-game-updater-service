e604660d89a984dc41c24fd9a2803a6b02c485a2
Gustavo Kei Amemiya <gustavokeia@gmail.com>
2024-09-21 13:35:26 -0300

    feat: using just one env var

--- index.js
+++ index.js
@@ -17,28 +17,7 @@ app.use((req, res, next) => {
 
 // Define a route for GET requests to "/"
 app.get('/', (req, res) => {
-  res.send({
-    1: {
-      clientDir: process.env.CLIENT_DIR1,
-      startCmd: process.env.START_CMD1,
-      launcherVer: process.env.LAUNCHER_VER1,
-      clientVer: process.env.CLIENT_VER1,
-      patchVer: process.env.PATCH_VER1,
-      launcherUrl: process.env.LAUNCHER_URL1,
-      clientUrl: process.env.CLIENT_URL1,
-      patchUrl: process.env.PATCH_URL1,
-    },
-    2: {
-      clientDir: process.env.CLIENT_DIR2,
-      startCmd: process.env.START_CMD2,
-      launcherVer: process.env.LAUNCHER_VER2,
-      clientVer: process.env.CLIENT_VER2,
-      patchVer: process.env.PATCH_VER2,
-      launcherUrl: process.env.LAUNCHER_URL2,
-      clientUrl: process.env.CLIENT_URL2,
-      patchUrl: process.env.PATCH_URL2,
-    }
-  });
+  res.send(process.env.JSON);
 });
 
 // Start the server
